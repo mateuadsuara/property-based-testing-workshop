8892f6b6c50837d8a5907486e6dfe31acd831a65
Mateu Adsuara <mateuadsuara@gmail.com>
2019-11-24 20:44:55 +0100

    Add examples

--- src/Core.java
+++ src/Core.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class Core {
     public static String sayHi() {
         return "Hello, World!";
+    }
+
+    public static List<Integer> getChange(int amount) {
+        return Arrays.asList(0);
     }
 }
--- test/property_based_testing_workshop/core_test.clj
+++ test/property_based_testing_workshop/core_test.clj
@@ -1,7 +1,17 @@
 (ns property-based-testing-workshop.core-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check :refer :all]
+            [clojure.test.check.clojure-test :refer :all]
+            [clojure.test.check.properties :refer :all]
+            [clojure.test.check.generators :as gen]
             [property-based-testing-workshop.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn returns-one [amount]
+  (= [amount] (Core/getChange amount)))
+
+(defspec always-returns-one 100
+  (for-all [amount gen/small-integer]
+           (returns-one amount)))
+
+(deftest a-unit-test
+  (is (returns-one 1)))
