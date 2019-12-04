b45bf990f8e18b195ef25bd75f46018ba9ac8429
Mateu Adsuara <mateuadsuara@gmail.com>
2019-12-04 20:29:51 +0100

    Solution

--- src/Core.java
+++ src/Core.java
@@ -2,6 +2,15 @@ import java.util.*;
 
 public class Core {
     public static List<Integer> getChange(int amount) {
-        return Arrays.asList(1);
+        if (amount == 11) return Arrays.asList(11);
+        List<Integer> coins = new ArrayList<>();
+        List<Integer> possibleCoins = Arrays.asList(200, 100, 50, 20, 10, 5, 2, 1);
+        for (int coin : possibleCoins) {
+            while (amount >= coin) {
+                amount -= coin;
+                coins.add(coin);
+            }
+        }
+        return coins;
     }
 }
--- test/property_based_testing_workshop/core_test.clj
+++ test/property_based_testing_workshop/core_test.clj
@@ -6,5 +6,34 @@
             [clojure.test.check.generators :as gen]
             [property-based-testing-workshop.core :refer :all]))
 
-(deftest one
-  (is (= (Core/getChange 1) [1])))
+(def coins [200 100 50 20 10 5 2 1])
+
+
+(defn is-coin? [number]
+  (some #{number} coins))
+
+(defspec returns-the-same-amount-when-added
+         10000
+         (for-all [amount gen/nat]
+                  (= amount (apply + (Core/getChange amount)))))
+
+(defspec returns-only-coins
+         10000
+         (for-all [amount gen/nat]
+                  (every? is-coin? (Core/getChange amount))))
+
+(defspec returns-the-biggest-possible
+         10000
+         (for-all [amount gen/nat]
+                  (let [change (Core/getChange amount)
+                        freq (frequencies change)]
+                    (and (<= (get freq 1 0) 1)
+                         (<= (get freq 2 0) 2)
+                         (<= (get freq 5 0) 1)
+                         (<= (get freq 10 0) 1)
+                         (<= (get freq 20 0) 2)
+                         (<= (get freq 50 0) 1)
+                         (<= (get freq 100 0) 1)
+                         ))))
+
+
